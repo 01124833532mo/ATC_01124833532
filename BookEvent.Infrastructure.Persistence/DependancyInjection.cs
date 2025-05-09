448bef599ad78fb04b2d07bedee58708afa3943c
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-05-09 18:09:21 +0300

    Implement For AuditInterceptor

--- BookEvent.Infrastructure.Persistence/DependancyInjection.cs
+++ BookEvent.Infrastructure.Persistence/DependancyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using BookEvent.Core.Domain.Contracts.Persestence.DbInitializers;
 using BookEvent.Infrastructure.Persistence._Data;
+using BookEvent.Infrastructure.Persistence._Data.Interceptors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,12 +13,14 @@ namespace BookEvent.Infrastructure.Persistence
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
 
-            services.AddDbContext<BookEventDbContext>((options) =>
+            services.AddDbContext<BookEventDbContext>((provider, options) =>
             {
                 options.UseLazyLoadingProxies()
-                .UseSqlServer(configuration.GetConnectionString("BookEventContext"));
+                .UseSqlServer(configuration.GetConnectionString("BookEventContext"))
+                .AddInterceptors(provider.GetRequiredService<AuditInterceptor>());
 
             });
+            services.AddScoped(typeof(AuditInterceptor));
             services.AddScoped(typeof(IBookEventDbInitializer), typeof(BookEventDbInitilzer));
 
             return services;
